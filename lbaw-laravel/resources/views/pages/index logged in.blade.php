781bf764d1e0a2d5bc3e193c09ce51ac4d7f819d
up201503995 <up201503995@fe.up.pt>
2018-05-04 19:33:46 +0100

    added support to Change The Points Color And Symbol depending on points being positive or negative - in questions

--- lbaw-laravel/resources/views/pages/index logged in.blade.php
+++ lbaw-laravel/resources/views/pages/index logged in.blade.php
@@ -114,18 +114,8 @@
                                                     <i id="downvoteArr-{{$question->question_id}}" class="fas fa-caret-down downvoteArr" onclick="voteInPost(event, -1)"></i>
                                                 </a>
                                             </div>
-                                            <div class="col-3 text-right">
-                                                <div id="points-{{$question->question_id}}" class="text-success">
-                                                    @if($question->question_points > -1)
-                                                        <i class="fas fa-plus" style="padding-right:5px;"></i>
-                                                        <b class="text-success">{{$question->question_points}} Points</b>
-                                                    @else
-                                                        <i class="fas fa-minus" style="padding-right:5px; color:red;"></i>
-                                                        <b class="text-danger">{{$question->question_points*-1}} Points</b>
-                                                    @endif
-                                                </div>
-                                            </div>
-                                                </div>
+                                            @include('pages.showQuestionPoints')
+                                         </div>
                                             </h5>
                                         </div>
                                         <div class="card-block border">
