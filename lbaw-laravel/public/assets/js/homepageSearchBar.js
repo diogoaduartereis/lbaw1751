02562098b445c023302a25fbf7232313c4291661
Dinis <up201504196@fe.up.pt>
2018-05-04 21:52:21 +0100

    added support for adding tag to search bar when its clicked on

--- lbaw-laravel/public/assets/js/homepageSearchBar.js
+++ lbaw-laravel/public/assets/js/homepageSearchBar.js
@@ -1,5 +1,11 @@
 function addTagToSearchBar(tag)
 {
-    Document.getElementById("");
+    let searchBar = document.getElementById("questionSearchBar");
+    let spaceIfNeeded = "";
+    if (searchBar.value == "")
+        spaceIfNeeded = "";
+    else if(searchBar.value[searchBar.value.length - 1] != " ")
+        spaceIfNeeded = " ";
+    searchBar.value = searchBar.value + spaceIfNeeded + "#" + tag;
     console.log(tag);
 }
