b41d5d50d82c9ec4736ce97f1f6319ad76007881
Dinis <up201504196@fe.up.pt>
2018-05-05 20:29:20 +0100

    removed useless code from reportPost.js

--- lbaw-laravel/public/assets/js/reportPost.js
+++ lbaw-laravel/public/assets/js/reportPost.js
@@ -6,13 +6,6 @@ function reportPost(event, postId)
 {
     let messageTextInput = document.getElementById('message');
     let reportReason = messageTextInput.value;
-    //get target element id
-    let element = event.target.id;
-
-    //get post id
-    let lhsIndex = element.indexOf("-") + 1;
-    let rhsIndex = element.indexOf("AsProcessed");
-    contactId = element.substring(lhsIndex, rhsIndex);
 
     //get csrf token
     let csrfToken = document.getElementById("csrf-token").innerHTML;
@@ -30,15 +23,4 @@ function reportPost(event, postId)
 function reponseArrived()
 {
     console.log("chegou");
-    if (this.responseText != "")
-        return;
-
-    document.getElementById("contact-" + contactId).remove();
-
-    //if no contacts to display
-    if(document.getElementsByClassName("contactsDiv").length == 0)
-      {
-        document.getElementById("ContactsList").insertAdjacentHTML('afterbegin', 
-            '<h3>No contacts to process</h3>');
-      }  
 }
