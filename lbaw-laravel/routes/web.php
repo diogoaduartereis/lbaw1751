45d3fcedc71ca8f5073a3ede932fa3c6b20c0bbd
Dinis <up201504196@fe.up.pt>
2018-05-06 18:55:44 +0100

    removed useless route

--- lbaw-laravel/routes/web.php
+++ lbaw-laravel/routes/web.php
@@ -96,7 +96,6 @@ Route::get('tags','PagesController@tags');
 
 //Contacts
 Route::get('contacts','ContactsController@contacts');
-Route::get('contacts/getAvailableSubjects','ContactsController@getAvailableSubjects');
 Route::post('contacts/submit','ContactsController@submitContactRequest');
 Route::get('contactsList','ContactsController@contactsList');
 Route::post('contacts/{id}/markAsProcessed','ContactsController@markContactAsProcessed');
