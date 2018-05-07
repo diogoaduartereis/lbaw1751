f610d89fc28130343e7e884122ad5730ac599588
Dinis <up201504196@fe.up.pt>
2018-05-08 00:42:08 +0100

    improved function getAllFaqs on Controllers/FaqController.php to be compatible with faq categories

--- lbaw-laravel/app/Http/Controllers/FaqController.php
+++ lbaw-laravel/app/Http/Controllers/FaqController.php
@@ -10,13 +10,21 @@ use Auth;
 
 class FaqController extends Controller
 {
+    //returns a map that associates the faq category to an array of faqs that correpsond to that category
     public static function getAllFaqs()
     {
-        $faqs = DB::table('faqentry')->select('name as category_name', 'question', 'answer')->get();
-        //for security purposes, do not return DB error information to the user possibly gibing 
-        if ($faqs)
-            return $faqs;
-        else
-            return "error";
+        $faqCats = DB::table('faqcategory')->select('name')->get();
+        $faqMap = array();
+        foreach ($faqCats as $faqCat)
+        {
+            $currFaq = DB::table('faqentry')->select('question', 'answer')
+                            ->join('category', 'category.id', '=', 'faqentry.category')
+                            ->where('category')->get();
+            if (!$currFaq)
+                return "error";
+            $faqMap[$faqCat->name] = $currFaq;
+        }
+        
+        return $faqMap;
     }
 }
