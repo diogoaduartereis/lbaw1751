f2c28746f1c1bbd3c3751b87af15c83074ef21ed
Dinis <up201504196@fe.up.pt>
2018-05-08 23:30:52 +0100

    added function to retrieve a variable number of first tags from the DB on TagController

--- lbaw-laravel/app/Http/Controllers/TagController.php
+++ lbaw-laravel/app/Http/Controllers/TagController.php
@@ -21,4 +21,17 @@ class TagController extends Controller
         $ret = DB::select('SELECT name FROM tag WHERE LOWER(name) LIKE :name LIMIT 5',['name' => $stringToMatch]);
         return json_encode($ret);
     }
+
+    public function getFirstXTags($numberOfTags)
+    {
+        $tags = DB::table('tag')
+                        ->select('name')
+                        ->take($numberOfTags)
+                        ->get();
+        //for security purposes, do not return DB error information to the user possibly gibing 
+        if (!$tags)
+            return "error";
+        else
+            return $tags;
+    }
 }
