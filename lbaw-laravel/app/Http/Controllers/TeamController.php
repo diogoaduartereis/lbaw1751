3e4f9dc0a9edc989263df2c0bc2cd51b44519a7a
Dinis <up201504196@fe.up.pt>
2018-05-08 18:46:39 +0100

    added function getMapWithAllTeamsToMembers on TeamController to get a map with all the teams name to team members names

--- lbaw-laravel/app/Http/Controllers/TeamController.php
+++ lbaw-laravel/app/Http/Controllers/TeamController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Redirect;
+use Hash;
+use Auth;
 
 class TeamController extends Controller
 {
-    //
+    public function getMapWithAllTeamsToMembers()
+    {
+        $teams = array();
+        $teamsNames = DB::table('team')->select('name')->get();
+        foreach ($teamsNames as $teamName)
+        {
+            $teamMembers = DB::table('team')
+                            ->join('teamtoteammember',  'team.id', '=', 'teamtoteammember.teamid')
+                            ->join('teammember', 'teamtoteammember.teammemberid', '=', 'teammember.id')
+                            ->select('teammember.name as member_name')
+                            ->where('team.name', '=', $teamName->name);
+            if (!$teamMembers)
+                return "error";
+            $teams[$teamName->name] = $teamMembers;
+        }
+
+        return $teams;
+    }
 }
