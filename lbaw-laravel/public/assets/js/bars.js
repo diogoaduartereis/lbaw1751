bfa2a0998e1763e2e9319bea2b84ad08113532d0
Dinis <up201504196@fe.up.pt>
2018-05-09 02:26:31 +0100

    added support for adding tag to the search bar when clicked on navbar, Tags dropdown menu

--- lbaw-laravel/public/assets/js/bars.js
+++ lbaw-laravel/public/assets/js/bars.js
@@ -128,6 +128,9 @@ $('.dropdown-menu a').on('click', function (event) {
 
     $(event.target).blur();
 
-    console.log(options);
+    console.log(event.target.childNodes[2]);
+    let tagName = event.target.childNodes[2].nodeValue;
+    tagName = tagName.substring(1, tagName.length);
+    addTagToSearchBar(tagName);
     return false;
 });
