7031101567ebc397202aec792495ab1dbfd2b732
Dinis <up201504196@fe.up.pt>
2018-05-12 02:40:15 +0100

    added support for inserting html that comes from server on loading by infinite scrolling

--- lbaw-laravel/public/assets/js/ajaxQuestionLoading.js
+++ lbaw-laravel/public/assets/js/ajaxQuestionLoading.js
@@ -9,7 +9,7 @@ function loadNextQuestionsFromServer()
     
     let numOfQuestionsToRetrieve = 5;
     let offset = questionsDiv.childNodes.length - 14; //14 is the number of html inside of the questions div that are not actually questions
-
+    console.log(offset);
     let ajaxRequest = new XMLHttpRequest();
     ajaxRequest.addEventListener("load", newQuestionsFromServerArrived);
     ajaxRequest.open("GET", "/getMostRecentPosts", true);
@@ -19,5 +19,13 @@ function loadNextQuestionsFromServer()
 
 function newQuestionsFromServerArrived()
 {
+    let questionsDiv = document.getElementById('Questions');
+
+//if the default questions are not currently being displayed but the searched are,
+//the div that must be operated, is the QuestionsFromSearchDiv
+if (questionsDiv.hidden == true)
+   questionsDiv = document.getElementById('QuestionsFromSearch');
+    questionsDiv.insertAdjacentHTML('beforeEnd', this.responseText);
+    //questionsDiv.appendChild(this.responseText);
     console.log(this.responseText);
 }
