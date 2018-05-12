6e773d243bdca9cfcac30c771b1c99b39e86924d
Dinis <up201504196@fe.up.pt>
2018-05-12 01:32:22 +0100

    fixed route type to get instead of post

--- lbaw-laravel/routes/web.php
+++ lbaw-laravel/routes/web.php
@@ -20,7 +20,7 @@ Route::get('404','PagesController@error404');
 Route::get('admin','PagesController@admin');
 
 //Get First X questions (used for viewing more questions using infinite scrolling)
-Route::post('getposts/{offset}/{numberOfPosts}','PostController@getXMostRecentQuestions');
+Route::get('getposts/{offset}/{numberOfPosts}','PostController@getXMostRecentQuestions');
 
 /*//Register
 Route::get('register','PagesController@register');
