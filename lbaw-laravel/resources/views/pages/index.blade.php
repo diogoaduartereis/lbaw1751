2012f9d841c085d2dd56358048b1be37c4a5d476
Davide Costa <up201503995@fe.up.pt>
2018-05-22 15:48:04 +0100

    fixed some W3C erros

--- lbaw-laravel/resources/views/pages/index.blade.php
+++ lbaw-laravel/resources/views/pages/index.blade.php
@@ -1,9 +1,10 @@
-<html class="no-js" lang="en">
+<!DOCTYPE html>
+<html>
 
     <head>
 
         <title>CodeHome</title>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
