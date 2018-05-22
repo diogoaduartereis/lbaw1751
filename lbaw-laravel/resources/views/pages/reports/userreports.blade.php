9fe634497211e44abfa7b2d91887b86cd5f2db94
Tiago José Sousa Magalhães <crakylps@gmail.com>
2018-05-22 14:36:17 +0100

    added file

---
+++ lbaw-laravel/resources/views/pages/reports/userreports.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CodeHome - User {{ $id }} Reports</title>
+
+    <link href="/assets/css/admin.css" rel="stylesheet">
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <link href="/assets/css/bars.css" rel="stylesheet">
+    <link href="/assets/css/common.css" rel="stylesheet">
+    <link href="/assets/css/about.css" rel="stylesheet">
+
+
+    <script src="/assets/js/jquery-1.11.1.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+
+
+    <style>
+        .breadcrumb {
+            padding: 5px 15px !important;
+        }
+
+        #accordion .card .card-header {
+            padding: 5px 15px !important;
+        }
+
+        @media screen and (max-width: 768px) {
+            .card {
+                margin-bottom: 1rem !important;
+            }
+        }
+    </style>
+
+
+</head>
+
+<body>
+
+<div id="wrap" class="wrapper">
+    @if(Auth::check())
+        @include('pages.sidebar')
+    @endif
+    <div id="content">
+        @if(Auth::check())
+            @include('pages.navbar logged in')
+        @else
+            @include('pages.navbar')
+        @endif
+
+        <div id="containerID">
+            <?php $i = 0 ?>
+            @foreach($reports as $report)
+                <?php $i++ ?>
+                <div id="postreport" class="row">
+                    <div class="col-sm-10 mx-auto" style="padding-left: 10vw">
+                        <div class="card border-dark">
+                            <div class="card-body">
+                                <h5 class="card-title border-bottom border-danger"><b>Report {{$i}}</b></h5>
+                                <p class="card-text">
+                                <ul style="list-style-type: none;">
+                                    <li><h5 class="card-text text-dark">Report Date:<?php
+                                            $dt = new DateTime($report->date);
+                                            $dt->setTimezone(new DateTimeZone('UTC'));
+                                            echo $dt->format('d-m-Y H:i:s');
+                                            ?></h5></li>
+                                    <li><h5 class="card-text text-dark">Report Reason:{{$report->reason}}</h5></li>
+                                    <li><h5 class="card-text text-dark">Reporter Username:{{$report->username}}</h5>
+                                    </li>
+                                    <li><h5 class="card-text text-dark">Reporter Email:{{$report->email}}</h5></li>
+                                    <li><h5 class="card-text text-dark">Reporter User Type:{{$report->type}}</h5>
+                                    </li>
+                                    <li><h5 class="card-text text-dark">Reporter User
+                                            Points:{{$report->points}}</h5></li>
+                                </ul>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+            @endforeach
+        </div>
+
+        <script src="/assets/js/bars.js"></script>
+</body>
+
+</html>
+
