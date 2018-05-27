6cbf81497678d0ba6c77fa5594b1c60cf707f0a6
Diogo Reis <up201505472@fe.up.pt>
2018-05-27 18:53:43 +0100

    added files

---
+++ lbaw-laravel/app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use Closure;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+    use ResetsPasswords;
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '';
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get the password reset validation rules.
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'pass_token' => 'required|confirmed|min:6',
+        ];
+    }
+
+    /**
+     * Get the password reset credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function credentials(Request $request)
+    {
+        return $request->only(
+            'email', 'pass_token', 'pass_token_confirmation', 'token'
+        );
+    }
+    
+    protected function validateNewPassword(array $credentials)
+    {
+        if (isset($this->passwordValidator)) {
+            list($password, $confirm) = [
+                $credentials['pass_token'],
+                $credentials['pass_token_confirmation'],
+            ];
+
+            return call_user_func(
+                $this->passwordValidator, $credentials
+            ) && $password === $confirm;
+        }
+
+        return $this->validatePasswordWithDefaults($credentials);
+    }
+    
+    protected function validatePasswordWithDefaults(array $credentials)
+    {
+        list($password, $confirm) = [
+            $credentials['pass_token'],
+            $credentials['pass_token_confirmation'],
+        ];
+ 
+        return $password === $confirm && mb_strlen($password) >= 6;
+    }
+}
+
+?>
