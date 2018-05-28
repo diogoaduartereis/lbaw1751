6e3141461adf6f5e4dfdc280fed4a24f24c46ce7
up201503995 <up201503995@fe.up.pt>
2018-05-28 22:17:31 +0100

    online help on bars and homepage done

--- lbaw-laravel/resources/views/pages/navbarloggedin.blade.php
+++ lbaw-laravel/resources/views/pages/navbarloggedin.blade.php
@@ -10,9 +10,9 @@ $tags = TagController::getFirstXTags(10);
     <button id="sidebarCollapse" class="btn border-0 bg-transparent" data-toggle="collapse" data-target="#sidebar" data-parent="#navbar"
             aria-expanded="false" aria-controls="sidebar">
         @if(preg_match('/https:\//', Auth::user()->img_path, $matches, PREG_OFFSET_CAPTURE))
-        <img class="img-responsive rounded-circle" width="30" height="30" src="{{Auth::user()->img_path}}" alt="Profile Picture" id="profPic2">
+        <img class="img-responsive rounded-circle" width="30" height="30" src="{{Auth::user()->img_path}}" title="Open/Close sidebar" alt="Profile Picture" id="profPic2">
         @else
-        <img class="img-responsive rounded-circle" width="30" height="30" src="/assets/img/users/{{Auth::user()->img_path}}" alt="Profile Picture" id="profPic2">
+        <img class="img-responsive rounded-circle" width="30" height="30" src="/assets/img/users/{{Auth::user()->img_path}}" title="Open/Close sidebar" alt="Profile Picture" id="profPic2">
         @endif
     </button>
     <a class="navbar-brand" title="CodeHome homepage" href="/">
