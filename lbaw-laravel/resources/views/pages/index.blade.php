fc7e0df51349e207558d03e7b41ad92d156b95c9
up201505472 <up201505472@fe.up.pt>
2018-05-29 01:38:29 +0100

    Fixed frontpage not logged in with everything the frontpage logged in already had

--- lbaw-laravel/resources/views/pages/index.blade.php
+++ lbaw-laravel/resources/views/pages/index.blade.php
@@ -14,95 +14,32 @@
         <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
         <link href="./assets/css/bars.css" rel="stylesheet">
         <link href="./assets/css/common.css" rel="stylesheet">
-        <link href="./assets/css/admin.css" rel="stylesheet">
         <link rel="stylesheet" href="./assets/css/Homepage/styles.css">
-        <link rel="stylesheet" href="./assets/css/HomepageNotLogged/style.css">
+        <link rel="stylesheet" href="./assets/css/Homepage/questions.css">
 
         <script src="./assets/js/jquery-1.11.1.min.js"></script>
         <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
         <script src="./assets/js/jquery.min.js"></script>
         <script src="./assets/js/popper.min.js"></script>
         <script src="./assets/js/homepageSearchBar.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="./assets/js/jquery.jscroll.min.js"></script>
 
         <meta name="google-site-verification" content="a30N-kL6X9vHRm5MlZ_OE720T-Vl8rKeKqC8x2_8AmM" />
     </head>
 
     <body>
-
         <div id="wrap" class="wrapper">
             <div id="content">
                 @include("pages.navbar")
-                <div id="classContainerID" class="container">
-                    <div class="row">
-                        <div id="searchInputForm" class="col-12">
-                            <div class="row" id="searchInput">
-                                <div class="col-8 mx-auto" style="margin-left:10vw;">
-                                    <form action="{{ url('/poster') }}" method="post">
-                                        <div class="input-group mb-3 mx-auto">
-                                            {{ csrf_field() }}
-                                            <input id="questionSearchBar" type="text" name="data" class="form-control border-dark" aria-label="Text input with segmented dropdown button" data-toggle="tooltip"
-                                                   data-placement="bottom" title="Search For Questions. Use the # before a word to add a tag to your search.">
-                                            <button type="button" class="btn btn-outline-dark">Search</button>
-                                        </div>
-                                    </form>    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-1">
+                <div id="containerID">
+                    <div id="contentID">
+                        @include('pages.index_questionsdiv')
                     </div>
                 </div>
-                <br>
-                <div id="contentID">
-                    @include('pages.index_questionsdiv')
-                    </div>
-                    <div id="changedContentID">
-                    </div>
             </div>
         </div>
 
-        <script>
-
-        $('#upvoteArr').mouseover(function () {
-            $("#upvoteArr").removeClass('text-secondary');
-            $("#upvoteArr").addClass('text-success');
-        })
-        $('#upvoteArr').mouseleave(function () {
-            $("#upvoteArr").addClass('text-secondary');
-            $("#upvoteArr").removeClass('text-success');
-        })
-
-
-        $('#downvoteArr').mouseover(function () {
-            $("#downvoteArr").removeClass('text-secondary');
-            $("#downvoteArr").addClass('text-danger');
-        })
-        $('#downvoteArr').mouseleave(function () {
-            $("#downvoteArr").addClass('text-secondary');
-            $("#downvoteArr").removeClass('text-danger');
-        })
-
-        $(function () {
-            $('.dropdown-menu li').on('click', function (event) {
-                var $checkbox = $(this).find('.checkbox');
-                if (!$checkbox.length) {
-                    return;
-                }
-                var $input = $checkbox.find('input');
-                var $icon = $checkbox.find('span.glyphicon');
-                if ($input.is(':checked')) {
-                    $input.prop('checked', false);
-                    $icon.removeClass('glyphicon-check').addClass('glyphicon-unchecked')
-                } else {
-                    $input.prop('checked', true);
-                    $icon.removeClass('glyphicon-unchecked').addClass('glyphicon-check')
-                }
-                return false;
-            });
-        });
-
-        </script>
         <script src="./assets/js/bars.js"></script>
         <script src="./assets/js/searchQuestion.js"></script>
     </body>
