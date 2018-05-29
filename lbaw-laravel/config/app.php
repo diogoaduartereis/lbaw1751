fa2657e6e9575dca6c4e277d705464fb625b4eeb
up201503995 <up201503995@fe.up.pt>
2018-05-29 19:49:34 +0100

    removed froala_editor_2.7.5.zip

--- lbaw-laravel/config/app.php
+++ lbaw-laravel/config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-		Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -174,7 +173,6 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
@@ -226,7 +224,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-		'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
