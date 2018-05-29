88747ffe3efc40177812dcb5ffa0b2ff66923e14
up201505472 <up201505472@fe.up.pt>
2018-05-29 04:57:39 +0100

    Password recovery now working with css. Missing background image

--- lbaw-laravel/resources/views/pages/ErrorPage/index.blade.php
+++ lbaw-laravel/resources/views/pages/ErrorPage/index.blade.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>CodeHome - Resource Not Found</title>
+        <title>CodeHome - 404</title>
 
 
         <link href="../assets/css/admin.css" rel="stylesheet">
@@ -39,8 +39,15 @@
                             <div class="container">
                                 <div class="row">
                                     <div class="col-sm-12 col-lg-12">
+                                        @if(!isset($name))
                                         <h1 id ="titleID" class="h1 text-primary">
-                                            Page Not Found </h1>
+                                            Page Not Found 
+                                        </h1>
+                                        @else
+                                        <h1 id ="titleID" class="h1 text-primary">
+                                            Access Denied
+                                        </h1>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
@@ -52,7 +59,14 @@
                             <div class="error-message-container">
 
                                 <div class="error-message" style="font-size: 150%;">
+                                    @if(isset($name) && $name == "Auth")
+                                        <p class="message">The link you tried to access isn't available for authenticated users. <br>
+                                            Please log out and try again.
+                                        </p>
+                                    @else
                                     <p class="message">The link you clicked may be broken or the page may have been removed.</p>
+                                    @endif
+
                                     <p class="small">
                                         Visit the
                                         <a href="/" ><b>homepage</b></a> or
