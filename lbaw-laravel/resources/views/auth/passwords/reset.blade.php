8679797cf561ba69e521863b273a5f37c6a7e22e
up201503995 <up201503995@fe.up.pt>
2018-05-29 21:46:35 +0100

    removed unused code

--- lbaw-laravel/resources/views/auth/passwords/reset.blade.php
+++ lbaw-laravel/resources/views/auth/passwords/reset.blade.php
@@ -20,7 +20,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default" style="margin: auto; width: 50%; border: 3px solid green; padding: 10px;">
+                <div class="panel panel-default">
                     <div class="panel-heading">Reset Password</div>
 
                     <div class="panel-body">
